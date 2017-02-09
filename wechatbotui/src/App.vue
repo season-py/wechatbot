2b9cb3351bee29ba91f8870f465bc60a366ff238
abitdot <haishan09@gmail.com>
2017-02-09 08:05:19 +0800

    ..

--- wechatbotui/src/App.vue
+++ wechatbotui/src/App.vue
@@ -1,44 +1,39 @@
 <template>
-  <el-row class="tac">
-    <el-col :span="4">
-      <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-        <el-submenu index="1">
-          <template slot="title"><i class="el-icon-message"></i>Navigator One</template>
-          <el-menu-item-group title="Group One">
-            <router-link class="tab-item active" to="/time-entries">
-              <el-menu-item index="1-1">item one</el-menu-item>
-            </router-link>
-            <el-menu-item index="1-2">item one</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="Group Two">
-            <el-menu-item index="1-3">item three</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <template slot="title">item four</template>
-            <el-menu-item index="1-4-1">item one</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2"><i class="el-icon-menu"></i>Navigator Two</el-menu-item>
-        <el-menu-item index="3"><i class="el-icon-setting"></i>Navigator Three</el-menu-item>
-      </el-menu>
-    </el-col>
-    <el-col :span="8">
-      <router-view></router-view>
-    </el-col>
-  </el-row>
-
+  <div>
+    <div class="headerWrapper">
+      <header>
+        <el-menu default-active="1" mode="horizontal" @select="handleSelect">
+          <el-menu-item index="1">Home</el-menu-item>
+          <el-menu-item index="3">Orders</el-menu-item>
+        </el-menu>
+      </header>
+    </div>
+    <div class="main-cnt">
+      <div class="page-container page-component">
+        <div class="el-row">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
     methods: {
-      handleOpen(key, keyPath) {
-        console.log(key, keyPath);
-      },
-      handleClose(key, keyPath) {
+      handleSelect(key, keyPath) {
         console.log(key, keyPath);
       }
     }
   }
 </script>
 
+<style>
+  .el-menu {
+    float: right;
+  }
+
+  body {
+    margin: 0;
+  }
+</style>
